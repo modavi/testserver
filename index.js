3fc0217e97056bd8b0afaf6bc7582cf48c0d7143
Morgan Davidson <morgan@Morgans-MacBook-Pro.local>
2023-10-11 23:20:44 -0700

    Add crash API

--- index.js
+++ index.js
@@ -1,11 +1,25 @@
+const { error } = require("console");
 const http = require("http");
+const url = require('url');
 
-const host = '192.168.1.90';
+const host = 'localhost';
 const port = 8000;
 
 const requestListener = function (req, res) {
+    var query = url.parse(req.url,true).query;
+    
+
     res.writeHead(200);
-    res.end("My first server!");
+
+    if(query['crash'])
+    {
+        res.end("CRASH");
+        throw error("BAD THINGS HAPPENED")
+    }
+    else
+    {
+        res.end("My first server!");
+    }
 };
 
 const server = http.createServer(requestListener);
